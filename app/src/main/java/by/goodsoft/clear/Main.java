7d23414d7672610ba21de9231e04058e3f46f619
Aliaksandr <developeralexshvets@gmail.com>
2018-03-18 18:35:38 +0300

    Clear app commit

---
+++ app/src/main/java/by/goodsoft/clear/Main.java
@@ -0,0 +1,17 @@
+package by.goodsoft.clear;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Aliaksandr Shvets on 18.03.2018.
+ */
+
+public class Main extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
